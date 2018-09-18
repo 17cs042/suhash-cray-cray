2208a23d89d1c127cf9453f9a3aa07a6499fc487
suhash cray cray <43338003+17cs042@users.noreply.github.com>
2018-09-18 14:37:49 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+string name[20];
+printf("\n enter the name:");
+scanf("%s,name);
+if
+{
+printf("%s is a alphabet",name);
+}
+else
+{
+printf("%s is not a alphabet",name);
+}
+getch();
+}
